faf839ebc722a80cf77a2102bad82b3fae272e4b
linhx13 <mylhx288@gmail.com>
2020-09-05 22:17:15 +0800

    update

---
+++ go/1080-insufficient-nodes-in-root-to-leaf-paths.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sufficientSubset(root *TreeNode, limit int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val < limit {
+			return nil
+		} else {
+			return root
+		}
+	}
+	root.Left = sufficientSubset(root.Left, limit-root.Val)
+	root.Right = sufficientSubset(root.Right, limit-root.Val)
+	if root.Left == root.Right {
+		return nil
+	} else {
+		return root
+	}
+}

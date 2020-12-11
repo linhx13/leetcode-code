8f966452b163659e54bafea6218407bfcdf4659a
linhx13 <mylhx288@gmail.com>
2020-12-11 12:35:50 +0800

    update

---
+++ code/712-minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
+        for j in range(1, n + 1):
+            dp[0][j] = dp[0][j - 1] + ord(s2[j - 1])
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                flag = (
+                    0
+                    if s1[i - 1] == s2[j - 1]
+                    else (ord(s1[i - 1]) + ord(s2[j - 1]))
+                )
+                dp[i][j] = min(
+                    dp[i - 1][j] + ord(s1[i - 1]),
+                    dp[i][j - 1] + ord(s2[j - 1]),
+                    dp[i - 1][j - 1] + flag,
+                )
+
+        return dp[m][n]
+
+
+if __name__ == "__main__":
+    s1 = "sea"
+    s2 = "eat"
+    print(Solution().minimumDeleteSum(s1, s2))

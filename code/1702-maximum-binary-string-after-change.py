7322206f0ae1ca425e04817e3fca5c4fc7e2333e
linhx13 <mylhx288@gmail.com>
2021-02-23 14:06:58 +0800

    update

---
+++ code/1702-maximum-binary-string-after-change.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumBinaryString(self, binary: str) -> str:
+        first = binary.find("0")
+        if first == -1:
+            return binary
+        cnt = binary.count("1", first)
+        n = len(binary)
+        return "1" * (n - cnt - 1) + "0" + "1" * cnt

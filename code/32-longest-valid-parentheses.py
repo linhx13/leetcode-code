e19014d8e2a18f8f1aa2235dbd0b5f0420a51839
linhx13 <mylhx288@gmail.com>
2021-05-06 11:00:00 +0800

    update

---
+++ code/32-longest-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        res = 0
+        stack = []
+        stack.append(-1)
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    res = max(res, i-stack[-1])
+        return res
